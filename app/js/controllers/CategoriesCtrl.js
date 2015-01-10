ff1d9c87d373845463e6d0b1d25bc9d76d918f15
Traqn <traqnzlatanov77@gmail.com>
2015-01-10 15:43:08 +0200

    Adding ride-sidebar panels views also implementing directives and controllers for categories and towns

---
+++ app/js/controllers/CategoriesCtrl.js
@@ -0,0 +1,11 @@
+app.controller('CategoriesCtrl', ['$scope', 'categoriesData', 'filter',function($scope, categoriesData, filter) {
+    categoriesData.getCategories()
+        .$promise
+        .then(function (data) {
+            $scope.categories = data;
+        });
+
+    $scope.categoryClicked = function categoryClicked(category) {
+        filter.filterByCategory(category);
+    }
+}]);
